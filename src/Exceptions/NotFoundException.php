081fc96e1b6bb13d06c12f9816f3f3aff4d2c043
Dhean Friendly Kasanda <dheanfriendlykasanda@gmail.com>
2023-08-26 01:54:32 +0700

    Update exception

--- src/Exceptions/NotFoundException.php
+++ src/Exceptions/NotFoundException.php
@@ -2,9 +2,19 @@
 
 namespace PsiMikroskil\Larashare\Exceptions;
 
+use Illuminate\Http\JsonResponse;
+use PsiMikroskil\Larashare\Http\Response;
 use RuntimeException;
 
 class NotFoundException extends RuntimeException
 {
-
+    /**
+     * Render error message
+     *
+     * @return JsonResponse
+     */
+    public function render(): JsonResponse
+    {
+        return Response::jsonError(404, $this->message);
+    }
 }
