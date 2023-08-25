a7965d00b501712de7d34876a35e87c2e5d0dc2e
Dhean Friendly Kasanda <dheanfriendlykasanda@gmail.com>
2023-08-25 15:23:16 +0700

    Add plain Service Provider for Larashare

---
+++ src/LarashareServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PsiMikroskil\Larashare;
+
+use Illuminate\Support\ServiceProvider;
+
+class LarashareServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
